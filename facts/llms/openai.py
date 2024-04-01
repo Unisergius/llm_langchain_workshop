10326f554b1a0adfc7f3723ca9ba06679afdeacd
shenriquesdev <Unisergius@gmail.com>
2024-04-01 23:09:42 +0100

    comment callbacks out of ChatOpenAI object

--- facts/llms/openai.py
+++ facts/llms/openai.py
@@ -3,5 +3,5 @@ from langchain_openai import ChatOpenAI
 
 def build_llm(chat_args):
     return ChatOpenAI(
-        callbacks=chat_args["callbacks"]
+        #callbacks=chat_args["callbacks"]
     )
